eebe234ad5410eb650c6de76db2e1a39a84e12ec
aryapc12 <aryavalsaraj98@gmail.com>
2023-11-17 15:49:13 +0530

    update navbar component to sfc

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,20 +1,30 @@
 import React, { Component } from "react";
 
-class Navbar extends Component {
-  render() {
-    return (
-      <nav className="navbar bg-body-tertiary">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="#">
-            Navbar
-            <span className="badge badge-pill badge-secondary m-2">
-              {this.props.totalCounters}
-            </span>
-          </a>
-        </div>
-      </nav>
-    );
-  }
-}
+//stateless functional component
+
+const Navbar = (props) => {
+  return (
+    <nav className="navbar bg-body-tertiary">
+      <div className="container-fluid">
+        <span className="navbar-brand mb-0 h1">
+          Total number of items
+          <span className="badge badge-pill badge-secondary m-2">
+            {props.totalCounters}
+          </span>
+        </span>
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;
+
+// class Navbar extends Component {
+//   render() {
+//     return (
+
+//     );
+//   }
+// }
+
+// export default Navbar;
